a0f34cb7822b615095f2952524241c9c59488ef3
Ryuryu128 <79790536+Ryuryu128@users.noreply.github.com>
2023-02-08 20:21:53 +0900

    Revert "Front network (#10)"
    This reverts commit 9b0488ef3bbee1c3701182006ed88c908d9a7885.

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -4,53 +4,55 @@ using UnityEngine;
 using UnityEngine.Networking;
 using MiniJSON;
 using System.IO;
-using System.Threading.Tasks;
 
-public class Network
+public class NetworkTest : MonoBehaviour
 {
+    // StartCoroutine(UnityWebRequestGETTest());
+
     private readonly string address = Network_Secret.getserveraddress();
     
     // POST
-    public async void PostNewRoom(string nickname)
+    IEnumerator PostNewRoom(int player_id)
     {
         WWWForm form = new WWWForm();
-        form.AddField("nickname", nickname);
-        UnityWebRequest request = UnityWebRequest.Post(address+"/rooms/", form);
-        
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        form.AddField("player_id", player_id);
+        UnityWebRequest request = UnityWebRequest.Post(address + "/rooms/",form);
+    
+        yield return request.SendWebRequest();
 
         if (request.error == null)
-        { Debug.Log(request.downloadHandler.text); }
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
         else
-        { Debug.Log("error"); }
+        {
+            Debug.Log("error");
+        }
     }
-    public async void PostNewPlayer(string nickname)
+    IEnumerator PostNewPlayer(string nickname)
     {
         WWWForm form = new WWWForm();
         form.AddField("nickname", nickname);
         UnityWebRequest request = UnityWebRequest.Post(address + "/players/", form);
 
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
 
         if (request.error == null)
-        { Debug.Log(request.downloadHandler.text); }
+        {
+            Debug.Log(request.downloadHandler.text);
+        }
         else
-        { Debug.Log("error"); }
+        {
+            Debug.Log("error");
+        }
     }
 
     // GET
-    public async Task<string> GetNewRoomCode(string nickname)
+    IEnumerator GetNewRoomCode(int player_id, string room_code)
     {
-        string room_code = null;
-        UnityWebRequest request = UnityWebRequest.Get(address + "/players/" + nickname); // 에러 시 주소 확인
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        UnityWebRequest request = UnityWebRequest.Get(address + "/players/" + player_id); // 에러 시 주소 확인
+        
+        yield return request.SendWebRequest();
 
         if (request.error == null)
         {
@@ -58,155 +60,90 @@ public class Network
             var jsonString = request.downloadHandler.text;
             var dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
             room_code = (string)dict["room_code"];
-            //File.WriteAllText("./data/data.txt", room_code); // 파일 입출력 확인 필요
+            File.WriteAllText("./data/data.txt", room_code); // 파일 입출력 확인 필요
         }
         else
         {
             Debug.Log("error");
         }
-
-        return room_code;
     }
-    public async Task<Dictionary<string, object>> GetRooms()
+    IEnumerator GetRooms(int player_id, string room_code)
     {
-        var dict = new Dictionary<string, object>();
+        UnityWebRequest request = UnityWebRequest.Get(address + "/players/" + player_id); // 에러 시 주소 확인
 
-        UnityWebRequest request = UnityWebRequest.Get(address + "/rooms/");
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
-
-        if (request.error == null)
-        {
-            Debug.Log(request.downloadHandler.text);
-            var jsonString = request.downloadHandler.text;
-            dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
-        }
-        else
-        {
-            Debug.Log("error");
-        }
-
-        return dict;
-    }
-    public async Task<int> GetTurninfo(string room_code)
-    {
-        int turninfo = 0;
-
-        UnityWebRequest request = UnityWebRequest.Get(address + "/rooms/" + room_code + "/turninfo/"); // 에러 시 주소 확인
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
 
         if (request.error == null)
         {
             Debug.Log(request.downloadHandler.text);
             var jsonString = request.downloadHandler.text;
             var dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
-            turninfo = (int)dict["turninfo"];
+            room_code = (string)dict["room_code"];
+            File.WriteAllText("./data/data.txt", room_code); // 파일 입출력 확인 필요
         }
         else
         {
             Debug.Log("error");
         }
-
-        return turninfo;
     }
-    public async Task<bool> GetisRoomFull(string room_code)
+    IEnumerator GetTurninfo(int player_id, string room_code)
     {
-        bool isfull = false;
+        UnityWebRequest request = UnityWebRequest.Get(address + "/players/" + player_id); // 에러 시 주소 확인
 
-        UnityWebRequest request = UnityWebRequest.Get(address + "/rooms/" + room_code);
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
 
         if (request.error == null)
         {
             Debug.Log(request.downloadHandler.text);
             var jsonString = request.downloadHandler.text;
             var dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
-            int player_num = (int)dict["player_num"];
-            if(player_num == 4)
-            { isfull = true; }
+            room_code = (string)dict["room_code"];
+            File.WriteAllText("./data/data.txt", room_code); // 파일 입출력 확인 필요
         }
         else
         {
             Debug.Log("error");
         }
-
-        return isfull;
     }
-    public async Task<bool> GetisNNExist(string nickname)
-    {
-        bool isexist = false;
 
-        UnityWebRequest request = UnityWebRequest.Get(address + "/players/" + nickname );
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
-
-        if (request.error == null)
-        {
-            Debug.Log(request.downloadHandler.text);
-            var jsonString = request.downloadHandler.text;
-            var dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
-            isexist = (bool)dict["isexist"];
-        }
-        else
-        {
-            Debug.Log("error");
-        }
-
-        return isexist;
-    }
-    
     // PUT
-    public async void PutPlayerToRoom(string room_code, string nickname)
+    IEnumerator PutPlayerToRoom(string room_code)
     {
-        UnityWebRequest request = UnityWebRequest.Put(address + "/rooms/" + room_code, nickname);
+        byte[] bytes = new byte[10];
+        WWWForm form = new WWWForm();
+        form.AddField("room_code", room_code);
+        UnityWebRequest request = UnityWebRequest.Put(address, bytes);
 
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
     }
-    public async void PutTurnEnd(string room_code)
+    IEnumerator PutTurnEnd(string room_code)
     {
-        UnityWebRequest request = UnityWebRequest.Put(address + "/" + room_code, room_code);
+        byte[] bytes = new byte[10];
+        WWWForm form = new WWWForm();
+        form.AddField("room_code", room_code);
+        UnityWebRequest request = UnityWebRequest.Put(address, bytes);
 
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
     }
-    public async void PutCardToPlayer(string nickname, int act, int card)  // str 추가 예정
+    IEnumerator PutCardToPlayer(string nickname)
     {
-        string str = "";
-        UnityWebRequest request = UnityWebRequest.Put(address, str);
+        byte[] bytes = new byte[10];
+        UnityWebRequest request = UnityWebRequest.Put(address, bytes);
 
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
     }
-    
+
     // DELETE
-    public async void DeleteRoom(string room_code)
+    IEnumerator DeleteRoom(string room_code)
     {
-        UnityWebRequest request = UnityWebRequest.Delete(address + "/rooms/" + room_code);
+        UnityWebRequest request = UnityWebRequest.Delete(address);
 
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
     }
-    public async void DeletePlayer(string nickname)
+    IEnumerator DeletePlayer(string room_code)
     {
-        UnityWebRequest request = UnityWebRequest.Delete(address + "/players/" + nickname);
+        UnityWebRequest request = UnityWebRequest.Delete(address);
 
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
+        yield return request.SendWebRequest();
     }
 }
