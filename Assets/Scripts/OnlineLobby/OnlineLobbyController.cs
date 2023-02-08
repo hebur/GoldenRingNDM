ba88960302ecb8c668abc1cdb7a631685744cc01
Seohyun Ryu <79790536+Ryuryu128@users.noreply.github.com>
2023-02-09 01:10:55 +0900

    feat: Player 닉네임 체크, Player Post

--- Assets/Scripts/OnlineLobby/OnlineLobbyController.cs
+++ Assets/Scripts/OnlineLobby/OnlineLobbyController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
+using Unity.VisualScripting;
 
 public class OnlineLobbyController : MonoBehaviour
 {
+    Network network;
+
     [SerializeField] private GameObject CreateRoom_UI, EnterCode_UI, PlayerNickname_UI;
+    [SerializeField] private TMP_InputField nicknameInputField;
 
     void Start()
     {
-
+        network= new Network();
     }
 
     public void BTN_CallBack()
@@ -19,9 +24,32 @@ public class OnlineLobbyController : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    public void BTN_CallPlayerNicknameCheck()
+    public async void BTN_CallPlayerNicknameCheck()
     {
-        
+        if (nicknameInputField.text == "")
+        {
+            // 팝업 띄워주기 TODO
+            Debug.Log("Nickname InputField is Emty!");
+            return;
+        }
+
+        // 이미 존재하는 닉네임인지 체크
+        string user_nickname = nicknameInputField.text;
+        var isExist = await network.GetisNNExist(user_nickname);
+
+        if (isExist)
+        {
+            // 팝업 띄워주기 TODO
+            Debug.Log("Nickname already Exists!");
+            return;
+        }
+            
+        network.PostNewPlayer(user_nickname);
+        PlayerNickname_UI.SetActive(false);
+
+        // 닉네임테스트
+        // nicknameTest1
+
     }
 
     public void BTN_CallCreateRoomEnter()
