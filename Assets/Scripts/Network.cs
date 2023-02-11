269048ce4036dbb95714ed9b45c82a771d631aff
Jimin Ryu <nick11967@snu.ac.kr>
2023-02-11 23:12:42 +0900

    Network.cs Struct Added (#27)
    * 골드를 사용할 수 있을 때만 사용 가능
    
    * Finally Front Network Finished

--- Assets/Scripts/Network.cs
+++ Assets/Scripts/Network.cs
@@ -1,30 +1,54 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using MiniJSON;
-using System.IO;
 using System.Threading.Tasks;
-using UnityEditor.UIElements;
 using System;
 
 public class Network
 {
     private readonly string address = Network_Secret.getserveraddress();
 
-    public class NN
+    public struct Player
     {
         public string nickname;
+        public string room_code;
+        public List<int> cards;
+
+        public Player(string nickname, string room_code, List<int> cards)
+        {
+            this.nickname = nickname;
+            this.room_code = room_code;
+            this.cards = cards;
+        }
+    }
+    public struct Room
+    {
+        public string code;
+        public string title;
+        public List<int> deck;
+        public int player_num;
+        public int turninfo;
+        public List<Player> players;
+
+        public Room(string code, string title, List<int> deck, int player_num, int turninfo, List<Player> players)
+        {
+            this.code = code;
+            this.title = title;
+            this.deck = deck;
+            this.player_num = player_num;
+            this.turninfo = turninfo;
+            this.players = players;
+        }
     }
 
     // POST
-    public async Task<string> PostNewRoom(int player_num)
+    public async Task<string> PostNewRoom(int player_num, string room_title)
     {
         string room_code = "";
 
         WWWForm form = new WWWForm();
         form.headers["Accept"] = "application/json";
-        string url = address + "/rooms/" + player_num;
+        string url = address + "/rooms/" + player_num + "/?room_title=" + room_title;
 
         UnityWebRequest request = UnityWebRequest.Post(url, form);
 
@@ -61,35 +85,9 @@ public class Network
     }
 
     // GET
-    /*public async Task<string> GetNewRoomCode(string nickname)
+    public async Task<List<Room>> GetRooms()
     {
-        string room_code = null;
-        string url = address + "/rooms/players/?nickname=" + nickname;
-        UnityWebRequest request = UnityWebRequest.Get(url);
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
-
-        if (request.error == null)
-        {
-            Debug.Log(request.downloadHandler.text);
-            room_code = request.downloadHandler.text;
-            //var jsonString = request.downloadHandler.text;
-            //var dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
-            //room_code = (string)dict["room_code"];
-            //File.WriteAllText("./data/data.txt", room_code); // 파일 입출력 확인 필요
-        }
-        else
-        {
-            Debug.Log("error");
-        }
-
-        return room_code;
-    }*/
-    public async Task<Dictionary<string, object>> GetRooms()
-    {
-        var dict = new Dictionary<string, object>();
+        List<Room> rooms = new List<Room>();
         string url = address + "/rooms/";
 
         UnityWebRequest request = UnityWebRequest.Get(url);
@@ -101,19 +99,21 @@ public class Network
         if (request.error == null)
         {
             Debug.Log(request.downloadHandler.text);
+
             string jsonString = request.downloadHandler.text;
-            /*int firstIndex = jsonString.IndexOf("\"");            
+            int firstIndex = jsonString.IndexOf("\"");
             jsonString = jsonString.Remove(firstIndex, "\"".Length);
-            int lastIndex = jsonString.IndexOf("\"");
-            jsonString = jsonString.Remove(lastIndex, "\"".Length);*/
-            dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
+            int lastIndex = jsonString.LastIndexOf("\"");
+            jsonString = jsonString.Remove(lastIndex, "\"".Length);
+
+            rooms = StoJRooms(jsonString);
         }
         else
         {
             Debug.Log("error");
         }
 
-        return dict;
+        return rooms;
     }
     public async Task<int> GetTurninfo(string room_code)
     {
@@ -138,9 +138,9 @@ public class Network
 
         return turninfo;
     }
-    public async Task<Dictionary<string, object>> GetRoomInfo(string room_code)
+    public async Task<Room> GetRoomInfo(string room_code)
     {
-        var dict = new Dictionary<string, object>();
+        Room room = new Room();
         string url = address + "/rooms/" + room_code;
 
         UnityWebRequest request = UnityWebRequest.Get(url);
@@ -153,18 +153,44 @@ public class Network
         {
             Debug.Log(request.downloadHandler.text);
             var jsonString = request.downloadHandler.text;
-            dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
+            room = StoJRoom(jsonString);
         }
         else
         {
             Debug.Log("error");
         }
 
-        return dict;
+        return room;
     }
-    public async Task<Dictionary<string, object>> GetPlayerInfo(string nickname)
+    public async Task<List<Player>> GetPlayers()
     {
-        var dict = new Dictionary<string, object>();
+        List<Player> players = new List<Player>();
+        string url = address + "/players/";
+
+        UnityWebRequest request = UnityWebRequest.Get(url);
+
+        var op = request.SendWebRequest();
+        while (!op.isDone)
+        { await Task.Yield(); }
+
+        if (request.error == null)
+        {
+            Debug.Log(request.downloadHandler.text);
+
+            string jsonString = request.downloadHandler.text;
+
+            players = StoJPlayers(jsonString);
+        }
+        else
+        {
+            Debug.Log("error");
+        }
+
+        return players;
+    }
+    public async Task<Player> GetPlayerInfo(string nickname)
+    {
+        Player player = new Player();
         string url = address + "/players/" + nickname;
 
         UnityWebRequest request = UnityWebRequest.Get(url);
@@ -177,60 +203,14 @@ public class Network
         {
             Debug.Log(request.downloadHandler.text);
             var jsonString = request.downloadHandler.text;
-            dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
+            //player = JsonUtility.FromJson<Player>(jsonString);
         }
         else
         {
             Debug.Log("error");
         }
 
-        return dict;
-    }
-    public async Task<Dictionary<string, object>> GetRoomInfo(string room_code, int x)
-    {
-        var dict = new Dictionary<string, object>();
-
-        UnityWebRequest request = UnityWebRequest.Get(address + "/rooms/" + room_code);
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
-
-        if (request.error == null)
-        {
-            Debug.Log(request.downloadHandler.text);
-            var jsonString = request.downloadHandler.text;
-            dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
-        }
-        else
-        {
-            Debug.Log("error");
-        }
-
-        return dict;
-    }
-    public async Task<Dictionary<string, object>> GetPlayerInfo(string nickname, string room_code)
-    {
-        var dict = new Dictionary<string, object>();
-
-        UnityWebRequest request = UnityWebRequest.Get(address + "/players/" + nickname);
-
-        var op = request.SendWebRequest();
-        while (!op.isDone)
-        { await Task.Yield(); }
-
-        if (request.error == null)
-        {
-            Debug.Log(request.downloadHandler.text);
-            var jsonString = request.downloadHandler.text;
-            dict = Json.Deserialize(jsonString) as Dictionary<string, object>;
-        }
-        else
-        {
-            Debug.Log("error");
-        }
-
-        return dict;
+        return player;
     }
     public async Task<bool> GetisRoomFull(string room_code)
     {
@@ -338,5 +318,181 @@ public class Network
         var op = request.SendWebRequest();
         while (!op.isDone)
         { await Task.Yield(); }
+    }
+
+
+    // JsonString to struct
+    private List<Room> StoJRooms(string str)
+    {
+        if (str.Length == 2)
+        {
+            return new List<Room>();
+        }
+
+        List<Room> rooms = new List<Room>();
+
+        int firstindex = -1, lastindex = -1;
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == '{')
+            {
+                count++;
+                if (firstindex == -1)
+                {
+                    firstindex = i;
+                }
+            }
+            else if (str[i] == '}')
+            {
+                count--;
+                if (count == 0)
+                {
+                    lastindex = i;
+                    rooms.Add(StoJRoom(str.Substring(firstindex, lastindex - firstindex + 1)));
+                    firstindex = lastindex = -1;
+                }
+            }
+        }
+
+        return rooms;
+    }
+    private Room StoJRoom(string str)
+    {
+        string temp;
+        int firstindex, lastindex;
+
+        string code;
+        string title;
+        List<int> deck;
+        int player_num;
+        int turninfo;
+        List<Player> players;
+
+        //deck
+        firstindex = str.IndexOf("[");
+        lastindex = str.IndexOf(']');
+        temp = str.Substring(firstindex + 1, lastindex - firstindex - 1);
+        deck = new List<int>();
+        if (temp.Length > 0)
+        {
+            string[] deckS = temp.Split(",");
+            for (int i = 0; i < deckS.Length; i++)
+            {
+                deck.Add(Int32.Parse(deckS[i]));
+            }
+        }
+
+        str = str.Substring(lastindex + 2);
+
+        //code
+        code = str.Substring(8, 6);
+
+        str = str.Substring(str.IndexOf(",") + 1);
+
+        //title
+        firstindex = str.IndexOf(",");
+        title = str.Substring(9, firstindex - 10);
+
+        str = str.Substring(firstindex + 1);
+
+        //player_num
+        firstindex = str.IndexOf(",");
+        player_num = Int32.Parse(str.Substring(13, firstindex - 13));
+
+        str = str.Substring(firstindex + 1);
+
+        //turninfo
+        firstindex = str.IndexOf(",");
+        turninfo = Int32.Parse(str.Substring(11, firstindex - 11));
+
+        firstindex = str.IndexOf("[");
+        str = str.Substring(firstindex);
+        firstindex = str.LastIndexOf("}");
+        str = str.Remove(firstindex);
+
+        //players
+        players = new List<Player>();
+        if (str.Length != 2)
+        {
+            firstindex = str.IndexOf("{");
+            while (firstindex >= 0)
+            {
+                firstindex = str.IndexOf("{");
+                lastindex = str.IndexOf("}");
+                if (firstindex < 0)
+                    break;
+                players.Add(StoJPlayer(str.Substring(firstindex, lastindex - firstindex + 1)));
+                str = str.Substring(lastindex + 1);
+            }
+        }
+
+        //create room with above data
+        Room room = new Room(code, title, deck, player_num, turninfo, players);
+
+        return room;
+    }
+    private List<Player> StoJPlayers(string str)
+    {
+        if (str.Length == 2)
+        {
+            return new List<Player>();
+        }
+
+        List<Player> players = new List<Player>();
+
+        int firstindex = -1, lastindex = -1;
+
+        firstindex = str.IndexOf("{");
+        while (firstindex >= 0)
+        {
+            firstindex = str.IndexOf("{");
+            lastindex = str.IndexOf("}");
+            if (firstindex < 0)
+                break;
+            Debug.Log(str.Substring(firstindex, lastindex - firstindex + 1));
+            players.Add(StoJPlayer(str.Substring(firstindex, lastindex - firstindex + 1)));
+            str = str.Substring(lastindex + 1);
+        }
+
+        return players;
+    }
+    private Player StoJPlayer(string str)
+    {
+        string temp;
+        int firstindex, lastindex;
+
+        string nickname;
+        string room_code;
+        List<int> cards;
+
+        //nickname
+        firstindex = str.IndexOf(",");
+        nickname = str.Substring(13, firstindex - 14);
+
+        str = str.Substring(firstindex + 1);
+
+        //room_code
+        room_code = str.Substring(13, 6);
+
+        str = str.Substring(str.IndexOf(",") + 1);
+
+        //cards
+        firstindex = str.IndexOf("[");
+        lastindex = str.IndexOf(']');
+        temp = str.Substring(firstindex + 1, lastindex - firstindex - 1);
+        cards = new List<int>();
+        if (temp.Length > 0)
+        {
+            string[] deckS = temp.Split(",");
+            for (int i = 0; i < deckS.Length; i++)
+            {
+                cards.Add(Int32.Parse(deckS[i]));
+            }
+        }
+        Player player = new Player(nickname, room_code, cards);
+
+        return player;
     }
 }
