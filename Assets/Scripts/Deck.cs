a041691523d7aefd44bd8a25e8d1444f18ff2a09
sforseohn <100199530+sforseohn@users.noreply.github.com>
2023-02-17 11:53:05 +0900

    카드 단계 덱에 반영하도록 수정 (#55)
    * 카드 단계 삭제
    
    * 카드 단계 덱에 반영하도록 수정

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -15,14 +15,14 @@ public class Deck : MonoBehaviour
     {
         LoadCards();
         if (doShuffle)
-            Shuffle(0, _cards.Count);
-        // Create_Deck();
+            // Shuffle(0, _cards.Count);
+            Create_Deck();
     }
 
     void LoadCards()
     {
         //StreamReader fs = new StreamReader(Path.Combine(Application.dataPath, "Resources/Json/cards.json"));
-        StreamReader fs = new StreamReader(Path.Combine(Application.streamingAssetsPath ,"cards_.json"));
+        StreamReader fs = new StreamReader(Path.Combine(Application.streamingAssetsPath, "cards_.json"));
         string str = fs.ReadToEnd();
         var json = JsonUtility.FromJson<CardLoadData>(str);
         _cards = new List<CardData>();
@@ -39,6 +39,29 @@ public class Deck : MonoBehaviour
             _cards.Add(newCard);
         }
     }
+
+    void Create_Deck()
+    {
+        int level = 0;
+        List<List<int>> num = new List<List<int>>(5); // [단계] [카드 번호]
+        for(int i = 0; i < 10; i++) num.Add(new List<int>());
+        foreach (var card in _cards)
+        {
+            level = card.CardNum / 100;
+            num[level].Add(card.CardNum); // 단계별 카드
+        }
+
+        int start = 0;
+        for (int lev = 0; lev < num.Count; lev++)
+        {
+            if (num[lev].Count != 0)
+            {
+                Shuffle(start, num[lev].Count);
+                start += num[lev].Count;
+            }
+        }
+    }
+
     void Shuffle(int start, int num)
     {
         int end = start + num - 1;
@@ -53,38 +76,6 @@ public class Deck : MonoBehaviour
             _cards[j] = tmp;
         }
     }
-
-    //void Shuffle(int num)
-    //{
-    //    for(int i = 0; i < num - 1 - 4; i++)
-    //    { 
-    //        int rnd = UnityEngine.Random.Range(i, num - 4);
-    //        exchange(i, rnd);
-    //    }
-    //    for(int i = 1; i <= 4; i++)
-    //    {
-    //        int rnd = UnityEngine.Random.Range((num - 4)/3, num - 4 + i);
-    //        exchange(num - 1 - 4 + i, rnd);
-    //    }
-    //}
-
-    /*
-    void Create_Deck()
-    {
-        int[] num = new int[] { 0, 14, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // 단계별 카드
-        int start = 0;
-
-        for (int i = 1; i < num.Length; i++)
-        {
-            if (num[i] != 0)
-            {
-                int[] level_deck = new int[num[i]];
-                Shuffle(start, num[i]);
-                start += num[i];
-            }
-        }
-    }
-    */
 }
 
 public class CardLoadData
